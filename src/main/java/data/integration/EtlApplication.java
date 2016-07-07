231f67f2bfff407f2e07364fa913760ba007fb7d
Jayesh M. Prajapati <jayesh.prajapati@smartstream-stp.com>
2016-07-07 19:14:46 +0530

    Added EtlApplication

---
+++ src/main/java/data/integration/EtlApplication.java
@@ -0,0 +1,12 @@
+package data.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtlApplication.class, args);
+	}
+}
---
+++ src/test/java/data/integration/EtlApplicationTests.java
@@ -0,0 +1,16 @@
+package data.integration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = EtlApplication.class)
+public class EtlApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
