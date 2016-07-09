d81ed59740910cf9f64f34139c3fe1c6f5af0da2
Jayesh M. Prajapati <jayesh.prajapati@smartstream-stp.com>
2016-07-09 11:19:10 +0530

    Added maven settings changes and minor corrections in beanio and aggregator

--- src/main/java/data/integration/eip/aggregator/StringBodyAggregationStrategy.java
+++ src/main/java/data/integration/eip/aggregator/StringBodyAggregationStrategy.java
@@ -30,19 +30,4 @@ public class StringBodyAggregationStrategy implements AggregationStrategy {
 		newIn.setBody(oldBody + newBody);
 		return newExchange;
 	}
-
-	public static void main(String[] args) {
-		String tradeRecord = "TC20150112132  072  132  C0795427  2000T972  2000T972                        R07   NG        20150600 00000000000029212015011200000001S   G     0001USD0000000000000000000000  E  C0795427  2000T972   E\r\nUC20150112132  072  132  C0795427  2000T972  NYX  GSAC AYGVF_SVK/AHGB-AYG000661489652ENERGY            N08N 2.921           126791                                                                     X";
-		String tradeRecord2 = "TC20150112132  072  132  C0795427  2000T972  2000T972                        R07   NG        20150600 00000000000029212015011200000001S   G     0001USD0000000000000000000000  E  C0795427  2000T972   E\nUC20150112132  072  132  C0795427  2000T972  NYX  GSAC AYGVF_SVK/AHGB-AYG000661489652ENERGY            N08N 2.921           126791                                                                     X";
-		System.out.println("trade: " + tradeRecord);
-		System.out.println();
-		String regex = "^(TC|TE).*(\r\n|\n).*";
-		System.out.println("regex '" + regex + "' = " + tradeRecord.matches(regex));
-		System.out.println();
-		System.out.println();
-		System.out.println("trade: " + tradeRecord2);
-		System.out.println();
-		System.out.println("regex '" + regex + "' = " + tradeRecord2.matches(regex));
-		System.out.println(tradeRecord.split("\r\n|\n"));
-	}
 }
